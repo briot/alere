025243af48117834c65f587a33bc4da783362679
Emmanuel Briot <emmanuel.briot@deepbluecap.com>
2020-07-29 18:15:49 +0200

    Change menu in the left sidebar to use rounded buttons

--- src/LeftSideBar/index.tsx
+++ src/LeftSideBar/index.tsx
@@ -1,54 +1,35 @@
 import React from 'react';
 import './LeftSideBar.css';
 
+interface RoundButtonProps {
+   icon: string;
+   text: string;
+   selected?: boolean;
+}
+
+const RoundButton: React.FC<RoundButtonProps> = p => {
+   return (
+      <a href='#a' className={`roundButton ${p.selected ? 'selected' : ''}`} >
+         <span className={`fa ${p.icon}`} />
+         <span>{p.text}</span>
+      </a>
+   );
+}
+
 interface LeftSideBarProps {
 }
 
 const LeftSideBar: React.FC<LeftSideBarProps> = p => {
    return (
       <div id='lsidebar'>
-         <ul>
-           <li>
-              <a href='#a'>
-                 <span className="fa fa-tachometer" /> Home
-              </a>
-           </li>
-           <li className='selected'>
-              <a href='#a'>
-                 <span className="fa fa-book" /> Ledger
-              </a>
-           </li>
-           <li>
-              <a href='#a'>
-                 <span className="fa fa-balance-scale" /> Budget
-              </a>
-           </li>
-           <li>
-              <a href='#a'>
-                 <span className="fa fa-bank" /> Investments
-              </a>
-           </li>
-           <li>
-              <a href='#a'>
-                 <span className="fa fa-pie-chart" /> Reports
-              </a>
-           </li>
-        </ul>
-
-        <h3>Favorite reports</h3>
-
-        <ul>
-           <li>
-              <a href='#a'>
-                 <span className="fa fa-line-chart" /> Custom 1
-              </a>
-           </li>
-           <li>
-              <a href='#a'>
-                 <span className="fa fa-line-chart" /> Custom 2
-              </a>
-           </li>
-        </ul>
+         <RoundButton icon="fa-tachometer" text="Home" />
+         <RoundButton icon="fa-book" text="Ledger" selected={true}/>
+         <RoundButton icon="fa-balance-scale" text="Budget" />
+         <RoundButton icon="fa-bank" text="Investments" />
+         <RoundButton icon="fa-pie-chart" text="Reports" />
+         <h3>Favorite reports</h3>
+         <RoundButton icon="fa-line-chart" text="Custom 1" />
+         <RoundButton icon="fa-line-chart" text="Custom 2" />
       </div>
    );
 }
