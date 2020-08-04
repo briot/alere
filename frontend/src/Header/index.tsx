e719508e3c86d9a7b7cf254eb842fb61b0079af1
Emmanuel Briot <briot.emmanuel@gmail.com>
2020-08-04 09:35:06 +0200

    Initial settings dialog

--- frontend/src/Header/index.tsx
+++ frontend/src/Header/index.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './Header.css';
 import Toolbar from 'ToolBar';
+import Settings from 'Settings';
+import { LedgerOptions } from 'Ledger';
 
 
 interface HeaderProps {
+   ledger: LedgerOptions;
+   setLedger: (opt: LedgerOptions) => void;
    title: string;
 }
 
 const Header: React.FC<HeaderProps> = p => {
    return (
       <div id='header'>
-         <Toolbar.Bar vertical={false} background={true}>
+         <Toolbar.Bar background={true}>
             <Toolbar.Button grows={true} label={p.title} />
             <Toolbar.Group>
                <Toolbar.Button title="Sync" icon="fa-refresh" />
-               <Toolbar.Button title="Settings" icon="fa-gear" />
+               <Settings ledger={p.ledger} setLedger={p.setLedger} />
             </Toolbar.Group>
          </Toolbar.Bar>
       </div>
