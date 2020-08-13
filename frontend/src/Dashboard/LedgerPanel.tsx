6e0dd811612e17356a2e413aa347cc058b5e1544
Emmanuel Briot <emmanuel.briot@deepbluecap.com>
2020-08-13 21:16:48 +0200

    ledger panel now has same settings as panel page

--- frontend/src/Dashboard/LedgerPanel.tsx
+++ frontend/src/Dashboard/LedgerPanel.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import Ledger, { LedgerProps } from 'Ledger';
+import { SplitMode, TransactionMode } from 'services/usePrefs';
+import { Checkbox, Select, Option } from 'Form';
 import { DateRange, DateRangePicker } from 'Dates';
 import { BaseProps, SettingsProps, DashboardModule } from 'Dashboard/Panels';
 
@@ -9,6 +11,14 @@ export interface LedgerPanelProps extends LedgerProps, BaseProps {
 
 const Settings: React.FC<SettingsProps<LedgerPanelProps>> = p => {
    const changeRange = (range: DateRange) => p.setData({ range });
+   const changeTrans = (trans_mode: string) =>
+      p.setData({ trans_mode: parseInt(trans_mode, 10) });
+   const changeSplit = (split_mode: string) =>
+      p.setData({ split_mode: parseInt(split_mode, 10) });
+   const changeBorders = (borders: boolean) => p.setData({ borders });
+   const changeExpand = (defaultExpand: boolean) => p.setData({ defaultExpand });
+   const changeValueColumn = (valueColumn: boolean) =>
+      p.setData({ valueColumn });
    return (
       <fieldset>
          <legend>Ledger</legend>
@@ -17,6 +27,76 @@ const Settings: React.FC<SettingsProps<LedgerPanelProps>> = p => {
             value={p.data.range || 'forever'}
             onChange={changeRange}
          />
+         <Checkbox
+             checked={p.data.borders}
+             onChange={changeBorders}
+             text="Show borders"
+         />
+         <Checkbox
+             checked={p.data.valueColumn}
+             onChange={changeValueColumn}
+             text="Deposit and paiements in same column"
+         />
+         <Checkbox
+             checked={p.data.defaultExpand}
+             onChange={changeExpand}
+             text="Expand rows by default"
+         />
+
+         <Select
+             text="Memos"
+             onChange={changeTrans}
+             value={p.data.trans_mode}
+         >
+             <Option
+                 text="Hide memos"
+                 value={TransactionMode.ONE_LINE}
+             />
+             <Option
+                 text="Show memos if not empty"
+                 value={TransactionMode.AUTO}
+             />
+             <Option
+                 text="Show memos always"
+                 value={TransactionMode.TWO_LINES}
+             />
+         </Select>
+
+         <Select
+             text="Splits"
+             onChange={changeSplit}
+             value={p.data.split_mode}
+         >
+             <Option
+                 text="Never show splits"
+                 value={SplitMode.HIDE}
+             />
+             <Option
+                 text="Show summary"
+                 value={SplitMode.SUMMARY}
+             />
+             <Option
+                 text="Show if more than two accounts"
+                 value={SplitMode.COLLAPSED}
+             />
+             <Option
+                 text="Multiple rows, no duplicate"
+                 value={SplitMode.OTHERS}
+             />
+             <Option
+                 text="Multiple rows"
+                 value={SplitMode.MULTILINE}
+             />
+         </Select>
+
+         <Select
+             text="Editing"
+             disabled={true}
+             value="inline"
+         >
+             <option>Inline</option>
+             <option>Separate window</option>
+         </Select>
       </fieldset>
    );
 }
