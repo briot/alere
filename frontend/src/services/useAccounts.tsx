3911688a0126d07e2cfe6b4f122fcb5707d0cad7
Emmanuel Briot <emmanuel.briot@deepbluecap.com>
2020-08-18 21:58:07 +0200

    Initial support for account selector

--- frontend/src/services/useAccounts.tsx
+++ frontend/src/services/useAccounts.tsx
@@ -14,8 +14,14 @@ export interface Account {
    forOpeningBalances: boolean;
 }
 
+export interface AccountTreeNode {
+   id: AccountId;
+   level: number;  // 0 for root, 1 for direct children, ...
+}
+
 export class AccountList {
    private accounts: Map<AccountId, Account>;
+   private tree: AccountTreeNode[];
 
    static async fetch() {
       const resp = await window.fetch('/api/account/list');
@@ -27,14 +33,52 @@ export class AccountList {
    constructor(acc: Account[]) {
       this.accounts = new Map();
       acc.forEach(a => this.accounts.set(a.id, a));
+
+      const children: Map<AccountId, AccountId[]> = new Map();
+      const roots: AccountId[] = [];
+      acc.forEach(a => children.set(a.id, []));
+      acc.forEach(a => {
+         if (a.parent) {
+            children.get(a.parent)!.push(a.id);
+         } else {
+            roots.push(a.id);
+         }
+      });
+
+      const cmpNode = (n1: AccountId, n2: AccountId) =>
+         this.accounts.get(n1)!.name
+         .localeCompare(this.accounts.get(n2)!.name);
+
+      children.forEach(n => n.sort(cmpNode));
+      roots.sort(cmpNode);
+
+      this.tree = [];
+      const add = (id: AccountId, level: number) => {
+         this.tree.push({ id, level });
+         children.get(id)!.forEach(id => add(id, level + 1));
+      }
+      roots.forEach(id => add(id, 0));
    }
 
    getAccount(id: AccountId): Account|undefined {
       return this.accounts.get(id);
    }
 
+   numAccounts(): number {
+      return this.accounts.size;
+   }
+
+   accountTree(): AccountTreeNode[] {
+      return this.tree;
+   }
+
    currencyId(id: AccountId): string {
       return this.getAccount(id)?.currencyId || '';
+   }
+
+   isStock(id: AccountId): boolean {
+      const ty = this.getAccount(id)?.accountType;
+      return ty === "Stock";
    }
 
    isIncomeExpense(id: AccountId): boolean {
@@ -49,6 +93,11 @@ export class AccountList {
       const pname = parent && parent.parent? this.fullName(parent) : undefined;
 
       return pname ? `${pname}:${acc.name}` : acc.name;
+   }
+
+   shortName(id: AccountId): string {
+      const acc = this.getAccount(id);
+      return acc ? acc.name : `account ${id}`;
    }
 
    name(id: AccountId): string {
