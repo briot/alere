be5b04cfaf9316c814e4eda55446343693fae7f5
Emmanuel Briot <briot.emmanuel@gmail.com>
2021-01-18 10:45:58 +0100

    update my account numbers

--- custom_kmy_kinds.py
+++ custom_kmy_kinds.py
@@ -5,24 +5,25 @@ from alere import models
 
 
 m = [
-    (150, models.AccountFlags.UNREALIZED_GAINS),   # plus-value potentielle
-    (10,  models.AccountFlags.PASSIVE_INCOME),     # alloc familiales
-    (9,   models.AccountFlags.PASSIVE_INCOME),     # dividendes
-    (170, models.AccountFlags.MISC_INCOME),        # heritage
-    (154, models.AccountFlags.PASSIVE_INCOME),     # interets
-    (7,   models.AccountFlags.WORK_INCOME),        # salaire Manu
-    (8,   models.AccountFlags.WORK_INCOME),        # salaire Marie
-    (63,  models.AccountFlags.WORK_INCOME),        # chomage
-    (163, models.AccountFlags.INCOME_TAX),         # URSAFF
-    (25,  models.AccountFlags.INCOME_TAX),         # impots revenu
-    (22,  models.AccountFlags.MISC_TAX),           # impots
-    (23,  models.AccountFlags.MISC_TAX),           # taxe fonciere
-    (24,  models.AccountFlags.MISC_TAX),           # taxe habitation
-    (156, models.AccountFlags.MISC_TAX),           # CSG
-    (160, models.AccountFlags.INVESTMENT),         # assurance-vie Anaelle
-    (125, models.AccountFlags.INVESTMENT),         # assurance-vie Manu Bourso
-    (128, models.AccountFlags.INVESTMENT),         # solesio
-    (127, models.AccountFlags.EQUITY),             # reconciliation
+    ( 7,   models.AccountFlags.WORK_INCOME),        # salaire Manu
+    ( 8,   models.AccountFlags.WORK_INCOME),        # salaire Marie
+    ( 9,   models.AccountFlags.PASSIVE_INCOME),     # dividendes
+    ( 10,  models.AccountFlags.PASSIVE_INCOME),     # alloc familiales
+    ( 22,  models.AccountFlags.MISC_TAX),           # impots
+    ( 23,  models.AccountFlags.MISC_TAX),           # taxe fonciere
+    ( 24,  models.AccountFlags.MISC_TAX),           # taxe habitation
+    ( 25,  models.AccountFlags.INCOME_TAX),         # impots revenu
+    ( 63,  models.AccountFlags.WORK_INCOME),        # chomage
+    ( 125, models.AccountFlags.INVESTMENT),         # assurance-vie Manu Bourso
+    ( 127, models.AccountFlags.EQUITY),             # reconciliation
+    ( 128, models.AccountFlags.INVESTMENT),         # solesio
+    ( 147, models.AccountFlags.UNREALIZED_GAINS),   # plus-value potentielle
+    ( 151, models.AccountFlags.PASSIVE_INCOME),     # interets
+    ( 153, models.AccountFlags.MISC_TAX),           # CSG
+    ( 157, models.AccountFlags.INVESTMENT),         # assurance-vie Anaelle
+    ( 160, models.AccountFlags.INCOME_TAX),         # URSAFF
+    ( 167, models.AccountFlags.MISC_INCOME),        # heritage
+    ( 180, models.AccountFlags.INVESTMENT),         # sequoia manu
 
 ]
 
