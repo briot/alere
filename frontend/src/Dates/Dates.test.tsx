42ed87e5118c6ab782c93b26d75797f2b3678f29
Emmanuel Briot <briot.emmanuel@gmail.com>
2021-11-17 10:18:57 +0100

    (Dates): fix tests

--- frontend/src/Dates/Dates.test.tsx
+++ frontend/src/Dates/Dates.test.tsx
@@ -3,27 +3,27 @@ import { addMonth, dateToDate, monthCount } from '@/Dates';
 test('adding months', () => {
    const d1 = new Date('2021-03-31 00:00:00Z');
    expect(d1.getTimezoneOffset()).toBe(-120);
-   addMonth(-1, d1);
-   expect(d1.getTimezoneOffset()).toBe(-60);
-   expect(d1).toStrictEqual(new Date('2021-02-28 01:00:00Z')); // dst
+   const d11 = addMonth(-1, d1);
+   expect(d11).toStrictEqual(new Date('2021-02-28 01:00:00Z')); // dst
+   expect(d11.getTimezoneOffset()).toBe(-60);
 
    const d2 = new Date('2021-05-31 03:00:00Z');
-   addMonth(-1, d2);
-   expect(d2).toStrictEqual(new Date('2021-04-30 03:00:00Z'));
+   const d22 = addMonth(-1, d2);
+   expect(d22).toStrictEqual(new Date('2021-04-30 03:00:00Z'));
 
    const d3 = new Date('2021-04-15 00:00:00Z');
    expect(d3.getTimezoneOffset()).toBe(-120);
-   addMonth(-1, d3);
-   expect(d3.getTimezoneOffset()).toBe(-60);
-   expect(d3).toStrictEqual(new Date('2021-03-15 01:00:00Z')); // ??? 01:00:00
+   const d33 = addMonth(-1, d3);
+   expect(d33.getTimezoneOffset()).toBe(-60);
+   expect(d33).toStrictEqual(new Date('2021-03-15 01:00:00Z')); // ??? 01:00:00
 
    const d4 = new Date('2021-05-31 03:00:00Z');
-   addMonth(1, d4);
-   expect(d4).toStrictEqual(new Date('2021-06-30 03:00:00Z'));
+   const d44 = addMonth(1, d4);
+   expect(d44).toStrictEqual(new Date('2021-06-30 03:00:00Z'));
 
    const d5 = new Date('2021-03-31 00:00:00Z');
-   addMonth(-12, d5);
-   expect(d5).toStrictEqual(new Date('2020-03-31 00:00:00Z'));
+   const d55 = addMonth(-12, d5);
+   expect(d55).toStrictEqual(new Date('2020-03-31 00:00:00Z'));
 });
 
 test('monthCount', () => {
