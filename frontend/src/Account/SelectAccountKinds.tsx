30e35364249da43ae8265a80b61113265ad1dcf0
Emmanuel Briot <briot.emmanuel@gmail.com>
2021-12-24 15:19:12 +0100

    (SelectAccountKinds): add tooltip

--- frontend/src/Account/SelectAccountKinds.tsx
+++ frontend/src/Account/SelectAccountKinds.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Option, Select, SelectProps } from '@/Form';
+import { Option, Select, SelectProps, Button } from '@/Form';
 import useAccounts, { AccountKindId } from '@/services/useAccounts';
 
 export interface SelectAccountKindProps
@@ -7,22 +7,84 @@ export interface SelectAccountKindProps
 }
 
 const SelectAccountKind: React.FC<SelectAccountKindProps> = p => {
-   const accounts = useAccounts();
+   const { accounts }  = useAccounts();
+
+   const tooltip = React.useCallback(
+      (id: AccountKindId) => {
+         const k = accounts.allAccountKinds[id];
+         return (
+            <div className="tooltip-base">
+               <table>
+                 <tbody>
+                    <tr>
+                       <th>Stock or Security</th>
+                       <td>{k.is_stock ? 'Y' : 'N'}</td>
+                    </tr>
+                    <tr>
+                       <th>Non-monetary asset (car,...)</th>
+                       <td>{k.is_asset ? 'Y' : 'N'}</td>
+                    </tr>
+                    <tr>
+                       <th>Show in Income/Expense</th>
+                       <td>{k.is_income_expense ? 'Y' : 'N'}</td>
+                    </tr>
+                    <tr>
+                       <th>Work income (salary,...)</th>
+                       <td>{k.is_work_income ? 'Y' : 'N'}</td>
+                    </tr>
+                    <tr>
+                       <th>Passive income (dividends,...)</th>
+                       <td>{k.is_passive_income ? 'Y' : 'N'}</td>
+                    </tr>
+                    <tr>
+                       <th>Realized income</th>
+                       <td>{k.is_realized_income ? 'Y' : 'N'}</td>
+                    </tr>
+                    <tr>
+                       <th>Unrealized income</th>
+                       <td>{k.is_unrealized_income ? 'Y' : 'N'}</td>
+                    </tr>
+                    <tr>
+                       <th>Expense</th>
+                       <td>{k.is_expense ? 'Y' : 'N'}</td>
+                    </tr>
+                    <tr>
+                       <th>Income tax</th>
+                       <td>{k.is_income_tax ? 'Y' : 'N'}</td>
+                    </tr>
+                    <tr>
+                       <th>Other taxes</th>
+                       <td>{k.is_other_tax ? 'Y' : 'N'}</td>
+                    </tr>
+                 </tbody>
+               </table>
+            </div>
+         );
+      },
+      [accounts]
+   );
 
    const options: Option<AccountKindId>[] = React.useMemo(
-      () => Object.values(accounts.accounts.allAccountKinds).map(
+      () => {
+         const obj = Object.values(accounts.allAccountKinds).map(
          k => ({
             value: k.id,
             text: k.name,
-         })),
-      [accounts.accounts.allAccountKinds]
+         }));
+         obj.sort((a, b) => a.text.localeCompare(b.text));
+         return obj;
+      },
+      [accounts.allAccountKinds]
    );
    return (
       <Select
          {...p}
          value={p.value ?? ''}
          options={options}
-      />
+         tooltip={tooltip}
+      >
+         <Button text="+" />
+      </Select>
    );
 }
 
